3184b8a57cb94ee6196d6cfb956d4c4a51144771
Aashim <thelastmanarmy37@gmail.com>
2024-12-11 20:23:36 +0545

    checkpoint 1 integrated openzepplin

--- contract/src/EHRManagement.sol
+++ contract/src/EHRManagement.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-contract EHRManagement {
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract EHRManagement is ReentrancyGuard {
     /*//////////////////////////////////////////////////////////////
                               DECLARATION
     //////////////////////////////////////////////////////////////*/
@@ -13,18 +15,20 @@ contract EHRManagement {
         DOCTOR,
         PATIENT
     }
+
     /*//////////////////////////////////////////////////////////////
-                             STATE VARIABLE
+                             STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
 
     mapping(address => Role) private s_roles;
-    uint256 private s_fileCounter = 0;
-
-    // Nested Mapping (doctor>fileId>file)
-    mapping(address => mapping(uint256 => File)) private s_addressToFiles; // a single user many have multiple file
-    //Nested Mapping (patient>fileId>doctor>true) or viceversa
+    mapping(address => uint256) private s_fileCounter;
+    mapping(address => mapping(uint256 => address)) private s_patientFiles;
+    mapping(address => mapping(uint256 => File)) private s_addressToFiles;
     mapping(address => mapping(uint256 => mapping(address => bool)))
         private s_authorize;
+    mapping(address => mapping(address => mapping(uint256 => bool)))
+        private s_paid;
+
     /*//////////////////////////////////////////////////////////////
                             ERRORS & EVENTS
     //////////////////////////////////////////////////////////////*/
@@ -35,6 +39,11 @@ contract EHRManagement {
         uint256 requiredBalance,
         uint256 submittedBalance
     );
+    error EHRManagement__PaymentNotFound(address patientAddress);
+    error EHRManagement__PaymentError(address doctorAddress);
+    error EHRManagement__AlreadyRegisteredAsDoctor();
+    error EHRManagement__AlreadyRegisteredAsPatient();
+    error EHRManagement__FileNotFound();
 
     event PermissionGranted(
         address indexed _to,
@@ -52,7 +61,8 @@ contract EHRManagement {
         address indexed patient,
         uint256 amount
     );
-    event RoleAssigned(address indexed user, Role indexed Role);
+    event RoleAssigned(address indexed user, Role indexed role);
+
     /*//////////////////////////////////////////////////////////////
                                MODIFIERS
     //////////////////////////////////////////////////////////////*/
@@ -63,6 +73,7 @@ contract EHRManagement {
         }
         _;
     }
+
     modifier onlyPatients() {
         if (s_roles[msg.sender] != Role.PATIENT) {
             revert EHRManagement__RolePermissionDenied(Role.PATIENT);
@@ -73,29 +84,73 @@ contract EHRManagement {
     /*//////////////////////////////////////////////////////////////
                                FUNCTIONS
     //////////////////////////////////////////////////////////////*/
+
     function registerUserAsDoctor() public {
+        if (s_roles[msg.sender] == Role.DOCTOR) {
+            revert EHRManagement__AlreadyRegisteredAsDoctor();
+        }
         s_roles[msg.sender] = Role.DOCTOR;
         emit RoleAssigned(msg.sender, Role.DOCTOR);
     }
 
     function registerUserAsPatient() public {
+        if (s_roles[msg.sender] == Role.PATIENT) {
+            revert EHRManagement__AlreadyRegisteredAsPatient();
+        }
         s_roles[msg.sender] = Role.PATIENT;
-        emit RoleAssigned(msg.sender, Role.DOCTOR);
+        emit RoleAssigned(msg.sender, Role.PATIENT);
+    }
+
+    function grantAccess(address _user, uint256 _fileCounter) public {
+        File memory file = s_addressToFiles[msg.sender][_fileCounter];
+        if (bytes(file.file).length == 0) {
+            revert EHRManagement__FileNotFound();
+        }
+        s_authorize[msg.sender][_fileCounter][_user] = true;
+        emit PermissionGranted(_user, msg.sender, _fileCounter);
     }
 
     function uploadReport(
         string memory _ipfsHash,
         uint256 _fee
     ) public onlyDoctor {
-        File memory newfile = File({file: _ipfsHash, fee: _fee});
-        s_addressToFiles[msg.sender][s_fileCounter] = newfile;
-        emit FileUploaded(msg.sender, s_fileCounter, _ipfsHash, _fee);
-        s_fileCounter++;
+        uint256 fileCounter = s_fileCounter[msg.sender];
+        File memory newFile = File({file: _ipfsHash, fee: _fee});
+        s_addressToFiles[msg.sender][fileCounter] = newFile;
+        emit FileUploaded(msg.sender, fileCounter, _ipfsHash, _fee);
+        s_fileCounter[msg.sender]++;
     }
 
-    function grantAccess(address _user, uint256 _fileCounter) public {
-        s_authorize[msg.sender][_fileCounter][_user] = true;
-        emit PermissionGranted(_user, msg.sender, _fileCounter);
+    function buyReport(
+        address _doctor,
+        uint256 _fileCounter
+    ) public payable onlyPatients nonReentrant {
+        if (s_roles[_doctor] != Role.DOCTOR) {
+            revert EHRManagement__PermissionDenied(msg.sender);
+        }
+        if (!s_authorize[_doctor][_fileCounter][msg.sender]) {
+            revert EHRManagement__PermissionDenied(msg.sender);
+        }
+        File memory file = s_addressToFiles[_doctor][_fileCounter];
+        if (file.fee > msg.value) {
+            revert EHRManagement__InsufficientPayment(file.fee, msg.value);
+        }
+
+        (bool sent, ) = payable(_doctor).call{value: msg.value}("");
+        if (!sent) {
+            revert EHRManagement__PaymentError(_doctor);
+        }
+
+        s_paid[msg.sender][_doctor][_fileCounter] = true;
+        assignFileToPatients(_doctor);
+
+        emit PaymentReceived(_doctor, msg.sender, msg.value);
+    }
+
+    function assignFileToPatients(address _doctor) public onlyPatients {
+        uint256 fileCounter = s_fileCounter[msg.sender];
+        s_patientFiles[msg.sender][fileCounter] = _doctor;
+        s_fileCounter[msg.sender]++;
     }
 
     function viewReport(
@@ -105,6 +160,9 @@ contract EHRManagement {
         if (s_authorize[_granter][_fileCounter][msg.sender] != true) {
             revert EHRManagement__PermissionDenied(msg.sender);
         }
-        return s_addressToFiles[_granter][s_fileCounter];
+        if (s_paid[msg.sender][_granter][_fileCounter] != true) {
+            revert EHRManagement__PaymentNotFound(msg.sender);
+        }
+        return s_addressToFiles[_granter][_fileCounter];
     }
 }
